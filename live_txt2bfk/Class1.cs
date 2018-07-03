6fe30fe20a1d23d5a3ce3d51796aea75105212c3
Peter Neal <peterneal150@gmail.com>
2018-07-03 16:48:39 -0500

    Rewrote the sorting algo (much, much better) Added transcoder from txt2bfk

--- live_txt2bfk/Class1.cs
+++ live_txt2bfk/Class1.cs
@@ -12,43 +12,212 @@ namespace live_txt2bfk
     {
 
         Queue<int> input = new Queue<int>();
+        Queue<int> used = new Queue<int>();
+        List<int> sorted = new List<int>();
 
+        int cellcount = -1;
+        List<Queue<int>> cells = new List<Queue<int>>();
+        Queue<int> item = new Queue<int>();
 
+        int x = 0; //cursor
+        List<int> output = new List<int>();
+        
         public DoCompute()
         {
+            cells.Add(item);
+
             while (true)
             {
+                int adjust = 0;
 
-                //Create an expanding distance table the printer can reference with each letter. Honestly, the only distances we should care about are the distances from each prior letter to the one we just entered. O(n^3) -> O(n^2).
+                //Create an expanding distance list the transcoder can reference with each letter.
 
                 ConsoleKeyInfo input_key = Console.ReadKey(true);
-                input.Enqueue(input_key.KeyChar);
-                int[][] dists = new int[input.Count][];
-                int[] input_as_array = input.ToArray();
-                for (int i = 0; i < input.Count; i++)
+                if (input_key.Key == ConsoleKey.Enter)
                 {
-                    dists[i] = new int[input.Count];
-                    for (int k = 0; k < input.Count; k++)
-                    {
-                        dists[i][k] = Math.Abs(input_as_array[i] - input_as_array[k]);
-                    }
+                    break;
                 }
-
-                /*
-                
-                for (int i = 0; i < input.Count; i++)
+                if (input_key.Key == ConsoleKey.NumPad0)
                 {
-                    Console.Write(input_as_array[i] + ": ");
-                    for (int k = 0; k < input.Count; k++)
+                    Console.Write("Sorted[] = ");
+                    for (int i = 0; i < sorted.Count; i++)
                     {
-                        Console.Write(dists[i][k] + ", ");
+                        Console.Write(sorted[i] + ", ");
                     }
                     Console.WriteLine();
+                    input_key = Console.ReadKey(true);
                 }
-                
+                input.Enqueue(input_key.KeyChar);
+                int[] input_as_array = input.ToArray();
+                int[] dists = new int[input.Count];
+                for (int i = 0; i < input.Count; i++)
+                {
+                    dists[i] = Math.Abs(input_as_array[input.Count - 1] - input_as_array[i]);
+                }
+
+
+                bool found = false;
+                for (int i = input.Count - 2; i >= 0; i--)
+                {
+                    if (dists[i] <= 8 && found == false)
+                    {
+                        sorted.Add(sorted[i]);
+                        cells[sorted[i]].Enqueue(input_as_array[input_as_array.Length - 1]);
+                        found = true;
+                    }
+                }
+                if (found == false)
+                {
+                    cellcount++;
+                    sorted.Add(cellcount);
+                    cells.Add(item);
+                    cells[cellcount].Enqueue(input_as_array[input_as_array.Length - 1]);
+                }
+
+
+
+
+                /*
+                Console.Write((char)input_as_array[input.Count-1] + ": ");
+                for (int i = 0; i < input.Count; i++)
+                {
+                    Console.Write(dists[i] + ", ");
+            
+                }
                 */
+                //Console.WriteLine();
+
+                //Begin Transcoding//
+                if (output.Count < input.Count)
+                {
+                    output.Add(0);
+                }
+                while (x != sorted.Last())
+                {
+                    if (x > sorted.Last())
+                    {
+                        x--;
+                        Console.Write("<");
+                        // decrement bfk pointer <
+                    }
+                    else
+                    {
+                        x++;
+                        Console.Write(">");
+                        //increment bfk pointer >
+                    }
+                }
+                int tmp = cells[x].Dequeue();
+                //Optimize a cell on its first call, kinda
+                if (output[x] == 0)
+                {
+
+                    int div = 1;
+
+                    int count = 0;
+
+
+                    for (int j = x; j < output.Count; j++)
+                    {
+                        if (output[j] != 0)
+                        {
+                            count++;
+                        }
+
+                    }
+                    //Console.Write("count = {0}", count);
+                    Console.Write(">"); //shift bf to next pointer to begin a counter.
+
+                    for (int j = 0; j < count; j++)
+                    {
+                        Console.Write(">"); //adjust for when we have to put the counter in a weird place. j is for adjust.
+                    }
+
+
+                    while (div == 1)
+                    {
+                        for (int j = 1; j < tmp / 2; j++)
+                        {
+                            if (tmp % j == 0 && j < tmp / div)
+                            {
+                                div = j;
+                            }
+                        }
+                        if (div == 1)
+                        {
+                            adjust++;
+                            tmp--;
+                        }
+                    }
+
+                    for (int j = 0; j < div; j++)
+                    {
+                        Console.Write("+"); //do the loop this many times
+                    }
+                    Console.Write("["); //begin loop
+                    Console.Write("<"); //go back to letter
+                    for (int j = 0; j < count; j++)
+                    {
+                        Console.Write("<"); //adjust for when we have to put the counter in a weird place. j is for adjust.
+                    }
+
+                    while (output[x] != tmp / div)
+                    {
+                        output[x]++;
+                        Console.Write("+"); //increment letter
+                    }
+
+                    Console.Write(">");//go back to the counter
+                    for (int j = 0; j < count; j++)
+                    {
+                        Console.Write(">"); //adjust for when we have to put the counter in a weird place. j is for adjust.
+                    }
+                    Console.Write("-]"); //decrement counter
+                    Console.Write("<"); //go back to letter
+                    for (int j = 0; j < count; j++)
+                    {
+                        Console.Write("<"); //adjust for when we have to put the counter in a weird place. j is for adjust.
+                    }
+                    output[x] = tmp;
+
+                }
+                else
+                {
+                    while (output[x] != tmp)
+                    {
+                        if (output[x] > tmp)
+                        {
+                            output[x]--;
+                            Console.Write("-");
+                            //decrement whatever output[x] is at
+                        }
+                        else
+                        {
+                            output[x]++;
+                            Console.Write("+");
+                            //increment whatever output[x] is at until it = the value we want
+                        }
+                    }
+                }
+                for (int j = 0; j < adjust; j++)
+                {
+                    Console.Write("+");
+                    output[x]++;
+
+                }
+                Console.Write(".");
+
 
             }
+
+            Console.Write("\nSorted[] = ");
+            for (int i = 0; i < sorted.Count; i++)
+            {
+                Console.Write(sorted[i] + ", ");
+            }
+
+            Console.WriteLine();
+            Console.ReadKey();
         }
 
     }
